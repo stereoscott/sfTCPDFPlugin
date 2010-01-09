c48d5dc3f87483c988e637d7049bacdad5e776dd
jonathan <jonathan@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-01-09 17:47:27 +0000

    sfTCPDF extended with callbacks for header and footer functions added magic functions
    git-svn-id: http://svn.symfony-project.com/plugins/sfTCPDFPlugin/branches/1.2@26436 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/sfTCPDF.class.php
+++ lib/sfTCPDF.class.php
@@ -10,17 +10,97 @@
 
 class sfTCPDF extends TCPDF
 {
-  /**
-   * Instantiate TCPDF lib.
-   *
-   * @param string $orientation
-   * @param string $unit
-   * @param string $format
-   * @param boolean $unicode
-   * @param string $encoding
-   */
-  public function __construct($orientation = 'P', $unit = 'mm', $format = 'A4', $unicode = true, $encoding = "UTF-8")
-  {
-    parent::__construct($orientation, $unit, $format, $unicode, $encoding);
-  }
+   /**
+    * When set this method is called as a header function.
+    * The variable must be a valid argument to call_user_func
+    * @var mixed
+    */
+   public $headerCallback = null;
+
+   /**
+    * When set this method is called as a header function.
+    * The variable must be a valid argument to call_user_func
+    * @var mixed
+    */
+   public $footerCallback = null;
+
+   /**
+    * Holds the data set via php magic methods
+    */
+   protected $userData = array();
+
+   /**
+    * Instantiate TCPDF lib.
+    *
+    * @param string $orientation
+    * @param string $unit
+    * @param string $format
+    * @param boolean $unicode
+    * @param string $encoding
+    */
+   public function __construct($orientation = 'P', $unit = 'mm', $format = 'A4', $unicode = true, $encoding = "UTF-8")
+   {
+      parent::__construct($orientation, $unit, $format, $unicode, $encoding);
+   }
+
+   /**
+    * This method is used to render the page header.
+    * It is automatically called by AddPage() and could be overwritten using a Callback.
+    * @access public
+    * @see $headerCallback
+    */
+   public function Header()
+   {
+      if ($this->print_header)
+         if (is_null($this->headerCallback))
+            parent::Header();
+         else
+            call_user_func($this->headerCallback, $this);
+   }
+
+   /**
+    * This method is used to render the page footer.
+    * It is automatically called by AddPage() and could be overwritten using a Callback.
+    * @access public
+    * @see $footerCallback
+    */
+   public function Footer()
+   {
+      if ($this->print_footer)
+         if (is_null($this->footerCallback))
+            parent::Footer();
+         else
+            call_user_func($this->footerCallback, $this);
+   }
+
+   public function __set($name, $value)
+   {
+      $this->userData[$name] = $value;
+   }
+
+   public function __get($name)
+   {
+      if (array_key_exists($name, $this->userData))
+      {
+         return $this->userData[$name];
+      }
+
+      $trace = debug_backtrace();
+      trigger_error(
+              'Undefined property via __get(): ' . $name .
+              ' in ' . $trace[0]['file'] .
+              ' on line ' . $trace[0]['line'],
+              E_USER_NOTICE);
+      return null;
+   }
+
+   public function __isset($name)
+   {
+      return isset($this->userData[$name]);
+   }
+
+   public function __unset($name)
+   {
+      unset($this->userData[$name]);
+   }
 }
