d57615e63c54dd75a1a63b95dddb9736a571c160
jonathan <jonathan@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-01-18 10:39:04 +0000

    [sfTCPDFPlugin] replaced is_null function (see HowToContributeToSymfony)
    git-svn-id: http://svn.symfony-project.com/plugins/sfTCPDFPlugin/branches/1.2@26803 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/sfTCPDF.class.php
+++ lib/sfTCPDF.class.php
@@ -12,53 +12,53 @@
 class sfTCPDF extends TCPDF
 {
   /**
-  * When set this method is called as a header function.
-  * The variable must be a valid argument to call_user_func
-  *
-  * @var mixed
-  */
+   * When set this method is called as a header function.
+   * The variable must be a valid argument to call_user_func
+   *
+   * @var mixed
+   */
   public $headerCallback = null;
 
   /**
-  * When set this method is called as a header function.
-  * The variable must be a valid argument to call_user_func
-  *
-  * @var mixed
-  */
+   * When set this method is called as a header function.
+   * The variable must be a valid argument to call_user_func
+   *
+   * @var mixed
+   */
   public $footerCallback = null;
 
- /**
-  * Holds the data set via php magic methods
-  */
+  /**
+   * Holds the data set via php magic methods
+   */
   protected $userData = array();
 
- /**
-  * Instantiate TCPDF lib.
-  *
-  * @param string $orientation
-  * @param string $unit
-  * @param string $format
-  * @param boolean $unicode
-  * @param string $encoding
-  */
+  /**
+   * Instantiate TCPDF lib.
+   *
+   * @param string $orientation
+   * @param string $unit
+   * @param string $format
+   * @param boolean $unicode
+   * @param string $encoding
+   */
   public function __construct($orientation = 'P', $unit = 'mm', $format = 'A4', $unicode = true, $encoding = "UTF-8")
   {
     parent::__construct($orientation, $unit, $format, $unicode, $encoding);
   }
 
- /**
-  * This method is used to render the page header.
-  * It is automatically called by AddPage() and could be overwritten using a Callback.
-  * 
-  * @access public
-  * @see $headerCallback
-  */
+  /**
+   * This method is used to render the page header.
+   * It is automatically called by AddPage() and could be overwritten using a Callback.
+   *
+   * @access public
+   * @see $headerCallback
+   */
   /*
   public function Header()
   {
     if ($this->print_header)
     {
-      if (is_null($this->headerCallback))
+      if (null !== $this->headerCallback)
       {
         parent::Header();
       }
@@ -70,19 +70,19 @@ class sfTCPDF extends TCPDF
   }
   */
 
- /**
-  * This method is used to render the page footer.
-  * It is automatically called by AddPage() and could be overwritten using a Callback.
-  *
-  * @access public
-  * @see $footerCallback
-  */
+  /**
+   * This method is used to render the page footer.
+   * It is automatically called by AddPage() and could be overwritten using a Callback.
+   *
+   * @access public
+   * @see $footerCallback
+   */
   /*
   public function Footer()
   {
     if ($this->print_footer)
     {
-      if (is_null($this->footerCallback))
+      if (null !== $this->footerCallback)
       {
         parent::Footer();
       }
@@ -92,15 +92,15 @@ class sfTCPDF extends TCPDF
       }
     }
   }
-   */
+  */
 
   /**
    * Magic setter.
-   * 
+   *
    * @param String $name
    * @param mixed $value
    */
-   /*
+  /*
   public function __set($name, $value)
   {
     $this->userData[$name] = $value;
@@ -112,7 +112,7 @@ class sfTCPDF extends TCPDF
    * @param String $name Name of data key to return
    * @return mixed
    */
-   /*
+  /*
   public function __get($name)
   {
     if (array_key_exists($name, $this->userData))
@@ -135,7 +135,7 @@ class sfTCPDF extends TCPDF
    * @param String $name
    * @return Boolean
    */
-   /*
+  /*
   public function __isset($name)
   {
     return isset($this->userData[$name]);
@@ -146,7 +146,7 @@ class sfTCPDF extends TCPDF
    *
    * @param String $name
    */
-   /*
+  /*
   public function __unset($name)
   {
     unset($this->userData[$name]);
